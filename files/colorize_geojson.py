d7552304e7b608cb27a42523782f917301339b93
Shane Brown <shane.brown@weathergroup.com>
2019-02-20 15:06:26 -0500

    new

--- files/colorize_geojson.py
+++ files/colorize_geojson.py
@@ -8,6 +8,8 @@ for i in range(0, len(data['features'])):
     ww = data['features'][i]['properties']['TYPE'] + data['features'][i]['properties']['SIG']
     if ww in ['FAA', 'FAY', 'FAW', 'FFS', 'FFA', 'FLY', 'FLW', 'FLA']:
         data['features'][i]['properties']['color'] = "#28B463"
+    elif ww in ['WSW', 'WSA', 'WWY']:
+        data['features'][i]['properties']['color'] = "#2876B4"
     else:
         data['features'][i]['properties']['color'] = "#ffffff"
         
